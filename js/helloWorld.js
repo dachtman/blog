64a2d7bcaa5fe3fbc154036b4ed6682f825172b4
dachtman <dachtman@gmail.com>
2013-09-04 09:38:57 -0500

    more file

---
+++ js/helloWorld.js
@@ -0,0 +1,21 @@
+var http = require('http'),
+    fs = require('fs');
+
+
+fs.readFile('../index.html', function (err, html) {
+    if (err) {
+        throw err; 
+    }       
+    http.createServer(function(request, response) {  
+        response.writeHeader(200, {"Content-Type": "text/html"});  
+        response.write(html);  
+        response.end();  
+    }).listen(8192);
+});
+console.log('Server running at http://108.228.60.70:8192/');
+
+/*var http = require('http');
+http.createServer(function (req, res) {
+  res.writeHead(200, {'Content-Type': 'text/plain'});
+  res.end('Hello World\n');
+}).listen();*/
